38557f12803ff6cd334b84f5019297bef08ed9cc
NataliZero <ball.diamond.ru@gmail.com>
2024-10-21 11:35:33 +0300

    updata
    Окончательный вариант кода

--- task_2.py
+++ task_2.py
@@ -1,7 +1,13 @@
 import requests
 
+# Параметры запроса
 params = {'userId': 1}
+
+# Отправляем GET-запрос с параметрами
 response = requests.get('https://jsonplaceholder.typicode.com/posts', params=params)
 
+# Проверяем статус-код
 print(response.status_code)
+
+# Выводим данные
 print(response.json())
